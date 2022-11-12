d6e3c7a44201a1d42e72cc01bdf3476f48f261ae
Ethan Hitchcock <49368798+EHitchc@users.noreply.github.com>
2022-11-13 12:04:06 +1300

    project setup

---
+++ LeetCodePlayground/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeetCodePlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
