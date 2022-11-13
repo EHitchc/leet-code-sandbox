4d832326705ddb9d52187dc6dab9b779756013df
Ethan Hitchcock <49368798+EHitchc@users.noreply.github.com>
2022-11-13 16:01:47 +1300

    Problem7 Integer Reversal

---
+++ LeetCodeSandbox/Problems/Problem7/ReverseInteger.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeSandbox.Problems.Problem7
+{
+    class ReverseInteger
+    {
+        // W/O library reverse methods
+        public int Reverse(int x)
+        {
+            bool isNegative = x < 0;
+
+            string numText = x.ToString();
+            StringBuilder sbReversed = new StringBuilder();
+
+            for (int i = numText.Length - 1; i >= 0; i--)
+                sbReversed.Append(numText[i]);
+
+            if (isNegative)
+            {
+                sbReversed.Remove(sbReversed.Length - 1, 1);
+                sbReversed.Insert(0, '-');
+            }
+
+            try
+            {
+                return int.Parse(sbReversed.ToString());
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        public int ReverseAlt(int x)
+        {
+            bool isNegative = x < 0;
+            char[] stringReversedArray = x.ToString().ToCharArray();
+            Array.Reverse(stringReversedArray);
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(stringReversedArray);
+
+            if (isNegative)
+            {
+                sb.Remove(sb.Length - 1, 1);
+                sb.Insert(0, '-');
+            }
+
+            try
+            {
+                return int.Parse(sb.ToString());
+            } catch
+            {
+                return 0;
+            }
+        }
+    }
+}
