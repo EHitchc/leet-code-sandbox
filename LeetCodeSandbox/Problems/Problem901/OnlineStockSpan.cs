6e9886cb9891e61871e647db1179909c68b93bea
Ethan Hitchcock <49368798+EHitchc@users.noreply.github.com>
2022-11-13 16:11:14 +1300

    Problem901 Online Stock Spanner

---
+++ LeetCodeSandbox/Problems/Problem901/OnlineStockSpan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeSandbox.Problems.Problem901
+{
+    class OnlineStockSpan
+    {
+        private Stack<int[]> priceHistory;
+
+        public OnlineStockSpan()
+        {
+            priceHistory = new Stack<int[]>();
+        }
+
+        public int Next(int price)
+        {
+            int count = 1;
+
+            while (priceHistory.Count > 0 && priceHistory.Peek()[0] <= price)
+                count += priceHistory.Pop()[1];
+
+            priceHistory.Push(new int[] { price, count });
+
+            return count;
+        }
+    }
+}
