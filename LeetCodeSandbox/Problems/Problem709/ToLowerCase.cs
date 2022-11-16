3742b7ae1d3c72d7381d1ae073d2fa4c6ca4df72
Ethan Hitchcock <49368798+EHitchc@users.noreply.github.com>
2022-11-16 22:28:49 +1300

    Problem709 To Lower Case

---
+++ LeetCodeSandbox/Problems/Problem709/ToLowerCase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeSandbox.Problems.Problem709
+{
+    class ToLowerCase
+    {
+        public string ToLowerCase(string s)
+        {
+            var sb = new StringBuilder();
+
+            foreach (char c in s)
+            {
+                if (c >= 'A' && c <= 'Z') sb.Append(Convert.ToChar(c + 32));
+                else sb.Append(c);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
